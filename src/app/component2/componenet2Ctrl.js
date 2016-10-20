ac935ddfe802c6ee561c6480150e1541430ce160
Nazarij Danyliak <n.danyliak@gmail.com>
2016-10-21 00:26:07 +0300

    some changes

---
+++ src/app/component2/componenet2Ctrl.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    angular.module('app.component2')
+        .controller('component1Ctr2', Component2Ctrl);
+
+
+    function Component2Ctrl() {
+        var vm = this;
+
+
+    }
+
+})();
---
+++ src/app/component2/tests/component2Ctrl.js
@@ -0,0 +1,15 @@
+describe('app.component2', function() {
+    beforeEach(module('app.app.component2'));
+
+    var $controller;
+
+    beforeEach(inject(function(_$controller_){
+        $controller = _$controller_;
+    }));
+
+    describe('Component2Ctrl', function() {
+        it('name', function() {
+            // ..........
+        });
+    });
+});
